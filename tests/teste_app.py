8ea26fd4301c93025a209b9e5ce9a3baa900d28f
gustavo <fgustavo.n@gmail.com>
2024-01-17 22:56:35 -0300

    first

---
+++ tests/teste_app.py
@@ -0,0 +1,11 @@
+from fast_zero.app import app
+from fastapi.testclient import TestClient
+
+
+def test_root_deve_retornar_200_e_ola_mundo():
+    client = TestClient(app)
+
+    response = client.get('/')
+
+    assert response.status_code == 200
+    assert response.json() == {'message': 'Olá Mundo!'}
